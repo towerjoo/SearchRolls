f25a707d419d1fdd0417ffacd0c7d2bc921c1b73
Zhu Tao <zhutao.iscas@gmail.com>
2013-08-23 23:45:44 +0800

    remove unused files

--- src/search.js
+++ src/search.js
@@ -1,21 +1,3 @@
-function getAvailabelSEs(){
-    return ["baidu", "baidu zhidao", "baidu baike", "google hongkong", "google taiwan", "wikipedia", ];
-}
-
-function getSearchQueries(se){
-    var queries = {
-        "baidu" : "http://www.baidu.com/s?wd=[KEYWORD]",
-        "baidu baike" : "http://baike.baidu.com/search/word?word=[KEYWORD]",
-        "baidu zhidao" : "http://zhidao.baidu.com/search?word=[KEYWORD]",
-
-        "google hongkong" : "http://www.google.com.hk/search?q=[KEYWORD]",
-        "google taiwan" : "http://www.baidu.com/s?wd=[KEYWORD]",
-
-        "wikipedia" : "http://en.wikipedia.org/w/index.php?title=Special:Search&search=[KEYWORD]"
-    };
-    return queries[se];
-}
-
 chrome.runtime.onInstalled.addListener(function(){
     var context = "selection";
     var title = "Search With";
